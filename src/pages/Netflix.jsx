b06d7b9db82a722c72a3e0fa690b36a2ea0794f6
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-11-25 02:19:43 +0530

    navbar+logoutFunctionality

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../component/Navbar";
 
 function Netflix(props) {
-  return <div></div>;
+  const [isScrolled, setisScrolled] = useState(false);
+  window.onscroll = () => {
+    setisScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.scroll = null);
+  };
+  return (
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
+  );
 }
 
 export default Netflix;
